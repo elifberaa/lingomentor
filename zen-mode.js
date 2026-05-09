98a2052d9f12e4977dce1ad826224db6b65873c9
Elif Çığrıkçı <elifcigrikci@hotmail.com>
2026-05-09 18:04:05 +0300

    Increase fatigue threshold to 0.75 to prevent over-sensitivity

--- zen-mode.js
+++ zen-mode.js
@@ -3,7 +3,7 @@
 (function () {
     // --- KONFİGÜRASYON ---
     const MODEL_URL = 'models/tfjs_model/model.json'; // Modelin yolu
-    const FATIGUE_THRESHOLD = 0.50; // Hassasiyet (0.0 ile 1.0 arası). Süre kontrolü olduğu için eşik daha da düşürüldü. 
+    const FATIGUE_THRESHOLD = 0.75; // Hassasiyet (0.0 ile 1.0 arası). Daha kararlı çalışması için eşik artırıldı. 
     const REQUIRED_FATIGUE_TIME_MS = 3000; // Gözlerin en az kaç milisaniye kapalı kalması gerektiği (3000 ms = 3 Saniye)
 
     let fatigueStartTime = 0;
